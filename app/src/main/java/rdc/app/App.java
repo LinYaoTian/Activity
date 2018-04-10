ce2735d228a97bb7a7075bad1869c9092da9906b
mayisheng <13104862372@163.com>
2018-04-10 22:57:03 +0800

    添加需要的库，配置Bmob

---
+++ app/src/main/java/rdc/app/App.java
@@ -0,0 +1,30 @@
+package rdc.app;
+
+import android.app.Application;
+
+import cn.bmob.v3.Bmob;
+import cn.bmob.v3.BmobConfig;
+
+/**
+ * Created by asus on 18-4-10.
+ */
+public class App extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+
+        BmobConfig config =new BmobConfig.Builder(this)
+                //设置appkey
+                .setApplicationId("4e50fe02409073ad4ad79f2cedcebde0")
+                //请求超时时间（单位为秒）：默认15s
+                .setConnectTimeout(30)
+                //文件分片上传时每片的大小（单位字节），默认512*1024
+                .setUploadBlockSize(1024*1024)
+                //文件的过期时间(单位为秒)：默认1800s
+                .setFileExpiration(2500)
+                .build();
+        Bmob.initialize(config);
+
+    }
+}
