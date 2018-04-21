f84baa400335f53865db99a24d56b6e32f88c626
Waxberry <295988788@qq.com>
2018-04-21 16:28:57 +0800

    写bug

--- app/src/main/java/rdc/activity/DetailActivity.java
+++ app/src/main/java/rdc/activity/DetailActivity.java
@@ -4,13 +4,14 @@ import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.net.Uri;
-import android.os.Bundle;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
+import android.view.WindowManager;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
@@ -18,17 +19,14 @@ import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
 import butterknife.BindView;
 import butterknife.OnClick;
 import cn.bmob.v3.BmobUser;
-import cn.bmob.v3.datatype.BmobRelation;
 import rdc.avtivity.R;
 import rdc.base.BaseActivity;
-import rdc.base.BasePresenter;
 import rdc.bean.Activity;
 import rdc.bean.Organization;
 import rdc.bean.User;
@@ -36,7 +34,6 @@ import rdc.contract.DetailContract;
 import rdc.presenter.DetailPresenter;
 import rdc.util.ACacheUtil;
 import rdc.util.ShareUtil;
-import rdc.util.UserUtil;
 
 /**
  * Created by WaxBerry on 2018/4/13.
@@ -45,42 +42,24 @@ import rdc.util.UserUtil;
 public class DetailActivity extends BaseActivity<DetailPresenter> implements DetailContract.IView {
 
     private String TAG = "DetailActivity";
-    @BindView(R.id.activity_detail_forward_imageView)
-    ImageView activity_detail_forward_imageView;
-    @BindView(R.id.activity_detail_poster_imageView)
-    ImageView activity_detail_poster_imageView;
-    @BindView(R.id.activity_detail_title_textView)
-    TextView activity_detail_title_textView;
-    @BindView(R.id.activity_detail_seeNum_textView)
-    TextView activity_detail_seeNum_textView;
-    @BindView(R.id.activity_detail_attendNum_textView)
-    TextView activity_detail_attendNum_textView;
-    @BindView(R.id.activity_detail_time_textView)
-    TextView activity_detail_time_textView;
-    @BindView(R.id.activity_detail_school_textView)
-    TextView activity_detail_school_textView;
-    @BindView(R.id.activity_detail_place_textView)
-    TextView activity_detail_place_textView;
-    @BindView(R.id.activity_detail_tag_textView)
-    TextView activity_detail_tag_textView;
-    @BindView(R.id.activity_detail_manager_relativeLayout)
-    RelativeLayout activity_detail_manager_relativeLayout;
-    @BindView(R.id.activity_detail_manager_imageView)
-    ImageView activity_detail_manager_imageView;
-    @BindView(R.id.activity_detail_manager_name_textView)
-    TextView activity_detail_manager_name_textView;
-    @BindView(R.id.activity_detail_manager_introduction_textView)
-    TextView activity_detail_manager_introduction_textView;
-    @BindView(R.id.activity_detail_add_textView)
-    TextView activity_detail_add_textView;
-    @BindView(R.id.activity_detail_content_textView)
-    TextView activity_detail_content_textView;
-    @BindView(R.id.activity_detail_consult_textView)
-    TextView activity_detail_consult_textView;
-    @BindView(R.id.activity_detail_signUp_textView)
-    TextView activity_detail_signUp_textView;
-    @BindView(R.id.toolbar)
-    Toolbar mToolbar;
+    @BindView(R.id.iv_forward_detail) ImageView activity_detail_forward_imageView;
+    @BindView(R.id.iv_poster_detail) ImageView activity_detail_poster_imageView;
+    @BindView(R.id.tv_title_detail) TextView activity_detail_title_textView;
+    @BindView(R.id.tv_seeNum_detail) TextView activity_detail_seeNum_textView;
+    @BindView(R.id.tv_attendNum_detail) TextView activity_detail_attendNum_textView;
+    @BindView(R.id.tv_time_detail) TextView activity_detail_time_textView;
+    @BindView(R.id.tv_school_detail) TextView activity_detail_school_textView;
+    @BindView(R.id.tv_place_detail) TextView activity_detail_place_textView;
+    @BindView(R.id.tv_tag_detail) TextView activity_detail_tag_textView;
+    @BindView(R.id.rv_manager_detail) RelativeLayout activity_detail_manager_relativeLayout;
+    @BindView(R.id.iv_manager_detail) ImageView activity_detail_manager_imageView;
+    @BindView(R.id.tv_manager_name_detail) TextView activity_detail_manager_name_textView;
+    @BindView(R.id.tv_manager_introduction_detail) TextView activity_detail_manager_introduction_textView;
+    @BindView(R.id.tv_add_detail) TextView activity_detail_add_textView;
+    @BindView(R.id.tv_content_detail) TextView activity_detail_content_textView;
+    @BindView(R.id.tv_consult_detail) TextView activity_detail_consult_textView;
+    @BindView(R.id.tv_signUp_detail) TextView activity_detail_signUp_textView;
+    @BindView(R.id.toolbar) Toolbar mToolbar;
     private Activity activity;
     private String objectId;
     private boolean hasSignUp = false;
@@ -88,12 +67,14 @@ public class DetailActivity extends BaseActivity<DetailPresenter> implements Det
     //    private List<User> focusUserList;
     private ACacheUtil mACacheUtil;
 
-
     @Override
     protected int setLayoutResID() {
         return R.layout.activity_detail;
     }
 
+    /**
+     * 初始化数据
+     */
     @Override
     protected void initData() {
         activity = new Activity();
@@ -101,12 +82,22 @@ public class DetailActivity extends BaseActivity<DetailPresenter> implements Det
         objectId = intent.getStringExtra("objectId");
         presenter.getDetail(objectId);
         mACacheUtil = ACacheUtil.get(getApplicationContext());
-
     }
 
+    /**
+     * 初始化界面
+     */
     @Override
     protected void initView() {
         initToolBar();
+        WindowManager wm = (WindowManager) this.getSystemService(Context.WINDOW_SERVICE);
+        int screenWidth  = wm.getDefaultDisplay().getWidth();
+        ViewGroup.LayoutParams lp = activity_detail_poster_imageView.getLayoutParams();
+        lp.width = screenWidth;
+        lp.height = ViewGroup.LayoutParams.WRAP_CONTENT;
+        activity_detail_poster_imageView.setLayoutParams(lp);
+        activity_detail_poster_imageView.setMaxWidth(screenWidth);
+        activity_detail_poster_imageView.setMaxHeight((int)(screenWidth * 0.7));
     }
 
     @Override
@@ -114,13 +105,19 @@ public class DetailActivity extends BaseActivity<DetailPresenter> implements Det
 
     }
 
-    @OnClick(R.id.activity_detail_forward_imageView)
+    /**
+     * 分享
+     */
+    @OnClick(R.id.iv_forward_detail)
     public void onForward() {
         ShareUtil.share(getSupportFragmentManager(), activity.getTitle() + "\n" + "时间 ： " + activity_detail_time_textView.getText().toString() +
                 "\n" + "地点 ： " + activity.getUniversity() + activity.getPlace());
     }
 
-    @OnClick(R.id.activity_detail_consult_textView)
+    /**
+     * 联系发布者
+     */
+    @OnClick(R.id.tv_consult_detail)
     public void onConsult() {
         if (activity.getManager().getMobilePhoneNumber() == null || activity.getManager().getMobilePhoneNumber().equals("")) {
             Toast.makeText(this, "该发布者还未设置联系电话！", Toast.LENGTH_SHORT).show();
@@ -131,17 +128,26 @@ public class DetailActivity extends BaseActivity<DetailPresenter> implements Det
         }
     }
 
-    @OnClick(R.id.activity_detail_add_textView)
+    /**
+     * 关注发布者
+     */
+    @OnClick(R.id.tv_add_detail)
     public void onAdd() {
         presenter.addFocus(activity.getManager().getObjectId(), hasFocus);
     }
 
-    @OnClick(R.id.activity_detail_signUp_textView)
+    /**
+     * 报名
+     */
+    @OnClick(R.id.tv_signUp_detail)
     public void onSignUp() {
         presenter.onSignUp(hasSignUp);
     }
 
-    @OnClick(R.id.activity_detail_manager_relativeLayout)
+    /**
+     * 跳转到发布者界面
+     */
+    @OnClick(R.id.rv_manager_detail)
     public void goToUserDetail() {
         Organization organization = new Organization();
         organization.setId(activity.getManager().getObjectId());
@@ -152,6 +158,10 @@ public class DetailActivity extends BaseActivity<DetailPresenter> implements Det
         OrganizationDetailsActivity.actionStart(this, organization);
     }
 
+    /**
+     * 设置详情的信息，增加该详情阅读数量，获取该用户关注列表
+     * @param activity
+     */
     @Override
     public void setDetail(Activity activity) {
         this.activity = activity;
@@ -190,6 +200,10 @@ public class DetailActivity extends BaseActivity<DetailPresenter> implements Det
         }
     }
 
+    /**
+     * 设置起始的时候用户是否已经报名该活动
+     * @param userList
+     */
     @Override
     public void setDetailAttcipator(List<User> userList) {
         if (userList == null || userList.size() == 0) {
@@ -212,6 +226,9 @@ public class DetailActivity extends BaseActivity<DetailPresenter> implements Det
         Log.d(TAG, "获取详情成功");
     }
 
+    /**
+     * 关注/取消关注成功
+     */
     @Override
     public void onSingOrUnSingUpSuccess() {
         int attendNum = Integer.parseInt(activity_detail_attendNum_textView.getText().toString());
@@ -289,6 +306,11 @@ public class DetailActivity extends BaseActivity<DetailPresenter> implements Det
         return true;
     }
 
+    /**
+     * 启动详情界面时调用该方法
+     * @param context
+     * @param objectId
+     */
     public static void actionStart(Context context, String objectId) {
         Intent intent = new Intent(context, DetailActivity.class);
         intent.putExtra("objectId", objectId);
@@ -306,7 +328,6 @@ public class DetailActivity extends BaseActivity<DetailPresenter> implements Det
 
     public void showConfirmDialog() {
         View view = getLayoutInflater().inflate(R.layout.dialog_detial_delete, null);
-
         AlertDialog dialog = new AlertDialog.Builder(this)
                 .setTitle("抱歉！")//设置对话框的标题
                 .setView(view)
@@ -321,7 +342,9 @@ public class DetailActivity extends BaseActivity<DetailPresenter> implements Det
         dialog.show();
     }
 
-
+    /**
+     * 获取Presenter实例
+     */
     @Override
     public DetailPresenter getInstance() {
         return new DetailPresenter();
